b883f4e0a4c00519577ecbed625c414eac49ea0a
Akakii <akakiichan@foxmail.com>
2020-04-24 00:44:58 +0800

    Homework9

---
+++ Homework9/Homework9/PageInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    class PageInfo
+    {
+        public int Index { get; set; }
+        public string URL { get; set; }
+        public string Status { get; set; }
+
+        public PageInfo(int index, string url, string status)
+        {
+            Index = index;
+            URL = url;
+            Status = status;
+        }
+    }
+}
